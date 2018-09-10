8247379566ac188b551e36fb862b110f1ac6c5d2
Unknown <wizardzrul@gmail.com>
2018-09-10 15:29:48 -0600

    Scripting Dog and Animal
    Working with classes. -_-

---
+++ 1610/Assets/Dog.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog : Animal
+{
+    void Awake ()
+    {
+        print("I am a dog");
+
+    }
+}
