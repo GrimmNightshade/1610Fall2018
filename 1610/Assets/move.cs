5426807e0ca51b6ef5812884c7a946008aaa960b
Unknown <wizardzrul@gmail.com>
2018-09-12 15:35:50 -0600

    Controlling Cube Movement
    Added both a translate and character movement script notes contained within the speed script.

---
+++ 1610/Assets/move.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+	public CharacterController Character;
+
+	public Vector3 RotateSpeed;
+	public Vector3 MoveSpeed;
+	public float Speed;
+
+	public void Update()
+	{
+		MoveSpeed.x = Speed * Input.GetAxis("Horizontal");
+		MoveSpeed *= Time.deltaTime;
+
+		Character.Move(MoveSpeed);
+	}
+
+
+}
+	
+//private void Update () {
+//transform.Translate(1,0,1);
+//MoveSpeed.x = Speed*Input.GetAxis("Horizontal");
+//MoveSpeed *= Time.deltaTime;
+//transform.Rotate(RotateSpeed);
+//transform.Translate(MoveSpeed);
+		
+//}
