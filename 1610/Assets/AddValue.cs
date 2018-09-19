85c03678c5b0b4d758539afbeae71c6deaf7530a
Unknown <wizardzrul@gmail.com>
2018-09-19 15:44:31 -0600

    Working with aswome

---
+++ 1610/Assets/AddValue.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class AddValue : ScriptableObject {
+
+	public FloatData ValueObj;
+	public FloatData Maxvalue;
+	public FloatData MinValue;
+
+	public UnityEvent EventMax;
+	public UnityEvent EventMin;
+
+
+	public void AddValueToObj (FloatData data)
+	 {
+		ValueObj.Value += data.Value;
+
+		if (ValueObj.Value >= Maxvalue.Value)
+		{
+			EventMax.Invoke();
+		}
+
+		if (ValueObj.Value <= MinValue.Value)
+		{
+			EventMin.Invoke();
+		}
+
+	}
+}
