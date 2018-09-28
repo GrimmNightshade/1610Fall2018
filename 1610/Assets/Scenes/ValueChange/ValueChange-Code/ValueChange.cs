171c6d7685d7fed78e6f82ef0738526be05b5d2b
Unknown <wizardzrul@gmail.com>
2018-09-27 20:24:22 -0600

    Math Function

---
+++ 1610/Assets/Scenes/ValueChange/ValueChange-Code/ValueChange.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class ValueChange : ScriptableObject
+{
+
+    public FloatData ValueObj;
+
+
+    public UnityEvent EventMax;
+    public UnityEvent EventMin;
+
+
+    public void AddValueToObj(FloatData data)
+    {
+        ValueObj.Value += data.Value;
+    }
+    public void SubractValueToObj(FloatData data)
+    {
+        ValueObj.Value -= data.Value;
+    }
+    public void MultiplyValueToObj(FloatData data)
+    {
+        ValueObj.Value *= data.Value;
+    }
+    public void DivideValueToObj(FloatData data)
+    {
+        ValueObj.Value /= data.Value;
+    }
+}
