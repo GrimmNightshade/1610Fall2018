6682ff7e5a2707f3c75c1c7bc4fbce32772b0d68
Unknown <wizardzrul@gmail.com>
2018-10-01 15:37:57 -0600

    Working with IF statements

---
+++ 1610/Assets/Scenes/Statements/IfStatements.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+    public bool LightOn;
+    public string Password;
+    public int A, B, C;
+    public FloatData WizardPower, PawnPower;
+    public NameID Lock, Key;
+    
+    void Update()
+    {
+        if (Lock == Key)
+        {
+            print("Open Door");
+        }
+
+        if(LightOn)
+        {
+            print("Day Time!");
+        }
+
+        if (Password == "OU812")
+        {
+            print("You are correct.");
+        }
+
+        if (C == A + B)
+        {
+            print(C);
+        }
+
+        if (WizardPower)
+        {
+            
+        }
+        if (PawnPower)
+        {
+            
+        }
+    }
+}
