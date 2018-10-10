033d21ac6382f3958109404a9d8377ba27dc2858
Unknown <wizardzrul@gmail.com>
2018-10-10 15:32:06 -0600

    10/10/18

---
+++ 1610/Assets/Scenes/CharacterMover/LocalVars.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalVars : MonoBehaviour
+{
+   string Name;
+
+   public void Call()
+   {
+      Name = "Bob";
+      
+   }
+
+   public void Call(string NewName)
+   {
+      print(NewName);
+      print(Name);
+   }
+
+}
---
+++ 1610/Assets/Scenes/CharacterMover/WhatCanSeeIt.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhatCanSeeIt : ScriptableObject {
+
+	// Use this for initialization
+	public float Health;
+	protected float PowerLevel;
+	public float EnergyLevel;
+	
+	private void AddHealth()
+	{
+		Health += 100;
+	}
+
+	protected void AddPower()
+	{
+		AddHealth();
+	}
+
+	public void AddEnergy()
+	{
+		
+	}
+
+	public class ICanSeeIt: MonoBehaviour
+	{
+
+
+		private void Start()
+		{
+			//newobject.EnergyLevel = 100;
+			//newobject.AddEnergy();
+		}
+	}
+
+//	public class Child : WhatCanSeeIt()
+	//{
+	//	void CallThings()
+	//	{
+		//	PowerLevel = 200;
+		//	EnergyLevel = 300;
+		//	AddPower();
+			//AddEnergy();
+		//}
+	//}
+}
