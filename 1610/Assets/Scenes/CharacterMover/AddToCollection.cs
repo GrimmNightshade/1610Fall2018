343c9a1df49ae85fa9d0c2f4b4cd83d6170f642a
Unknown <wizardzrul@gmail.com>
2018-10-15 15:38:31 -0600

    Day 10/15/18

---
+++ 1610/Assets/Scenes/CharacterMover/AddToCollection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Schema;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class AddToCollection : ScriptableObject
+{
+
+    public ObectCollection Collection;
+    
+    public void Call(ScriptableObject obj)
+    {
+        Collection.ObjectList.Add(obj);
+    }
+}
