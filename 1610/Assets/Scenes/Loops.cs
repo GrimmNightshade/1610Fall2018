b51171da43bb12f5c88be103faaa9d220efaabd2
Unknown <wizardzrul@gmail.com>
2018-10-24 15:08:10 -0600

    Thing

---
+++ 1610/Assets/Scenes/Loops.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+
+    public string[] Names;
+    public GameObject[] Things;   
+    public GameObject[] Thing; 
+    void Start()
+    {
+
+        
+        for (int i = 0; i <= Names.Length; i++)
+        {
+            print(Names[i]);
+        }
+
+        foreach (GameObject Thing in Things)
+        {
+            
+        }
+
+       /* while (count != 25)
+        {
+            var count = 0;
+            count += 1;
+            print("Word " + count);   
+        }
+        
+        */
+        
+    }
+}
