82f20420a5af22a89a2cc3548aa017e695b8f03d
Unknown <wizardzrul@gmail.com>
2018-10-31 15:10:52 -0600

    Weapons Stuffs

---
+++ 1610/Assets/Scenes/Gamestates/SwitchWeapons.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SwitchWeapons : MonoBehaviour {
+	
+	public Weapons.WeaponTypes GameState;
+
+	public UnityEvent Starting, Loading, Playing, Dieing, Ending;
+	//refrencing the C# script gameStates
+
+	private void Update()
+	{
+		switch (GameState)
+		{
+			case Weapons.WeaponTypes.Starting:
+				print("Starting the game now");
+				break;
+             
+             
+			case Weapons.WeaponTypes.Playing:
+				print("Play The game!");
+				break;
+            
+            
+			case Weapons.WeaponTypes.Loading:
+				print("Loading.....");
+				break;
+
+			case Weapons.WeaponTypes.Dieing:
+				print("Your Dead");
+				break;
+            
+			case Weapons.WeaponTypes.Ending:
+				print("Ending Game");
+				break;
+		}
+	}
+}
---
+++ 1610/Assets/Scenes/Gamestates/Weapons.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour {
+
+		public enum WeaponTypes
+		{
+			Trishot,
+			SprayShot,
+			RefelctShot,
+			BoomerangeShot
+		}
+
+		
+    
+}
+
