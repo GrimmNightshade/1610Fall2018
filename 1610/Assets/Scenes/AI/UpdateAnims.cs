ce7cbbdb2682362183c3c655f9152cacf8f8abd8
Unknown <wizardzrul@gmail.com>
2018-11-14 15:23:31 -0700

    Animation Script

---
+++ 1610/Assets/Scenes/AI/UpdateAnims.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateAnims : MonoBehaviour
+{
+
+	private Animator anims;
+	public float speed;
+
+	private void Start()
+	{
+		anims = GetComponent<Animator>();
+	}
+
+	void Update()
+	{
+		anims.SetFloat("speed", speed);
+	}
+
+}
